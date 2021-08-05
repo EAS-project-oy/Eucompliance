14bdd3d171c6468bf292bef896429ad6e36ebb8c
AndrijOnufrijchuk <furaw2@gmail.com>
2021-08-05 11:24:30 +0300

    Initial commit

---
+++ Model/Source/Attributes.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EasEucompliance\Model\Source;
+
+use Eas\Eucompliance\Model\Config\Configuration;
+use Magento\Framework\Data\OptionSourceInterface;
+use Magento\Eav\Model\ResourceModel\Entity\Attribute\CollectionFactory as AttributeCollectionFactory;
+
+/**
+ * Copyright © EAS Project Oy. All rights reserved.
+ */
+class Attributes implements OptionSourceInterface
+{
+
+    /**
+     * @var AttributeCollectionFactory
+     */
+    private AttributeCollectionFactory $collectionFactory;
+
+    /**
+     * Attributes constructor.
+     * @param AttributeCollectionFactory $collectionFactory
+     */
+    public function __construct(
+        AttributeCollectionFactory $collectionFactory
+    ) {
+        $this->collectionFactory = $collectionFactory;
+    }
+
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $attributes = [];
+        $collection = $this->collectionFactory->create()->setEntityTypeFilter(Configuration::PRODUCT_ENTITY_TYPE);
+        foreach ($collection->getData() as $attribute) {
+            $attributes[$attribute[Configuration::ATTRIBUTE_CODE]] = $attribute[Configuration::ATTRIBUTE_CODE];
+        }
+
+        return $attributes;
+    }
+}
---
+++ view/frontend/web/js/view/shipping-eas-mixin.js
@@ -0,0 +1,103 @@
+/**
+ * Copyright © EAS Project Oy. All rights reserved.
+ */
+define([
+    'jquery',
+    'mage/url',
+    'underscore',
+    'Magento_Ui/js/form/form',
+    'ko',
+    'Magento_Customer/js/model/customer',
+    'Magento_Customer/js/model/address-list',
+    'Magento_Checkout/js/model/address-converter',
+    'Magento_Checkout/js/model/quote',
+    'Magento_Checkout/js/action/create-shipping-address',
+    'Magento_Checkout/js/action/select-shipping-address',
+    'Magento_Checkout/js/model/shipping-rates-validator',
+    'Magento_Checkout/js/model/shipping-address/form-popup-state',
+    'Magento_Checkout/js/model/shipping-service',
+    'Magento_Checkout/js/action/select-shipping-method',
+    'Magento_Checkout/js/model/shipping-rate-registry',
+    'Magento_Checkout/js/action/set-shipping-information',
+    'Magento_Checkout/js/model/step-navigator',
+    'Magento_Ui/js/modal/modal',
+    'Magento_Checkout/js/model/checkout-data-resolver',
+    'Magento_Checkout/js/checkout-data',
+    'uiRegistry',
+    'mage/translate',
+    'Magento_Checkout/js/model/shipping-rate-service'
+], function (
+    $,
+    url,
+    _,
+    Component,
+    ko,
+    customer,
+    addressList,
+    addressConverter,
+    quote,
+    createShippingAddress,
+    selectShippingAddress,
+    shippingRatesValidator,
+    formPopUpState,
+    shippingService,
+    selectShippingMethodAction,
+    rateRegistry,
+    setShippingInformationAction,
+    stepNavigator,
+    modal,
+    checkoutDataResolver,
+    checkoutData,
+    registry,
+    $t
+) {
+    'use strict';
+
+    var mixin = {
+        setShippingInformation: function () {
+            if (this.validateShippingInformation()) {
+                quote.billingAddress(null);
+                checkoutDataResolver.resolveBillingAddress();
+                registry.async('checkoutProvider')(function (checkoutProvider) {
+                    var shippingAddressData = checkoutData.getShippingAddressFromData();
+
+                    if (shippingAddressData) {
+                        checkoutProvider.set(
+                            'shippingAddress',
+                            $.extend(true, {}, checkoutProvider.get('shippingAddress'), shippingAddressData)
+                        );
+                    }
+                });
+                setShippingInformationAction().done(
+                    function () {
+                        $.ajax({
+                            type: "POST",
+                            url: url.build('eas'),
+                            cache: true,
+                            dataType: 'json',
+                            context: this,
+                            data: {},
+                            success: function (data, textStatus) {
+                                if (data.redirect) {
+                                    window.location.href = data.redirect;
+                                } else if(data.error) {
+                                    let messageContainer = registry.get('checkout.errors').messageContainer;
+                                    messageContainer.addErrorMessage({
+                                        message: $t(data.error)
+                                    })
+                                }
+                                else if (data.disabled) {
+                                    stepNavigator.next();
+                                }
+                            }
+                        });
+                    }
+                );
+            }
+        }
+    };
+
+    return function (target) {
+        return target.extend(mixin);
+    }
+});
